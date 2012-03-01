07b36cd9d31171324d15af20577a1cb0eb45c221
Simon Michael <simon@joyful.com>
2012-03-01 18:56:11 +0000

    simplify site.hs

--- site.hs
+++ site.hs
@@ -2,15 +2,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-
 website build script
-ghc --make hakyll.hs && ./hakyll build
+ghc --make site.hs && ./site build
 -}
 
--- import Prelude hiding (id)
--- import Control.Category (id)
 import Control.Arrow ((>>>)) --, (***), arr)
 import Hakyll
-
--- siteurl = "http://joyful.com/fungen"
 
 main:: IO ()
 main = hakyll $ do
@@ -19,22 +15,4 @@ main = hakyll $ do
         compile $ pageCompiler
             >>> applyTemplateCompiler "site.hamlet"
             >>> relativizeUrlsCompiler
-
-    -- match "images/*" $ do
-    --     route   idRoute
-    --     compile copyFileCompiler
-
-    -- match "files/*" $ do
-    --     route   idRoute
-    --     compile copyFileCompiler
-
-    -- match "css/*" $ do
-    --     route   idRoute
-    --     compile compressCssCompiler
-
     match "*.hamlet" $ compile templateCompiler
-
-    -- match "robots.txt" $ do
-    --     route   idRoute
-    --     compile copyFileCompiler
-
