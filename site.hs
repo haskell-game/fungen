116f14feb237fb0c56b68e0d96b77a34a4e909dd
Simon Michael <simon@joyful.com>
2013-08-06 00:27:55 +0000

    docs: update site for current hakyll

--- site.hs
+++ site.hs
@@ -2,15 +2,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 {- hakyll script, builds website in ./_site/ -}
 
-import Control.Arrow ((>>>))
+import Control.Applicative
+import Data.List
+import Data.Monoid
+import Debug.Trace
 import Hakyll
+import qualified Text.Blaze.Html5                 as H
+import qualified Text.Blaze.Html5.Attributes      as A
+import           Text.Blaze.Html                  (toHtml, toValue, (!))
+import           Text.Blaze.Html.Renderer.String  (renderHtml)
 
 main:: IO ()
 main = hakyll $ do
-  match "*.hamlet" $ compile templateCompiler
+  match "site.html" $ compile templateCompiler
   match "*.md" $ do
     route   $ setExtension "html"
-    compile $ pageCompiler >>> applyTemplateCompiler "site.hamlet" >>> relativizeUrlsCompiler
+    compile $ pandocCompiler >>= loadAndApplyTemplate "site.html" defaultContext >>= relativizeUrls
   match "old-site/**" $ do
     route   idRoute
     compile copyFileCompiler
